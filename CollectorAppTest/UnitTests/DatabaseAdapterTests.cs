dc45bf7c7d5d3f81345cd845713aaf8c242faa9c
ToastyTad <toastytadgit@gmail.com>
2018-11-01 20:47:39 -0400

    added database test.

--- CollectorAppTest/UnitTests/DatabaseAdapterTests.cs
+++ CollectorAppTest/UnitTests/DatabaseAdapterTests.cs
@@ -1,21 +1,43 @@
-﻿using CollectorApp.Models;
+﻿using CollectorApp.Adapters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace CollectorAppTest
 {
     [TestClass]
     public class DatabaseAdapterTests
     {
+        private const string FIRST_ADDITION = "First Addition";
+        private const string SECOND_ADDITION = "Second Addition";
+
+        private IDatabaseAdapter _databaseAdapter;
+        private List<string> _possibleData;
+
         [TestInitialize]
         public void Initialize()
         {
-            // anything needed before each test (common code among all tests)
+            _databaseAdapter = DatabaseAdapterFactory.Instance.GetDatabaseAdapter();
+            _possibleData = new List<string> { FIRST_ADDITION, SECOND_ADDITION };
         }
 
-        [TestCleanup]
-        public void Cleanup()
+        [TestMethod]
+        public void AddGetDataTest()
         {
-            // anything needed after each test (common code among all tests)
+            _databaseAdapter.AddData(FIRST_ADDITION);
+            var data = _databaseAdapter.GetData();
+            for (int i = 0; i < data.Count; i++)
+            {
+                Debug.WriteLine(data[i]);
+                Assert.AreEqual(_possibleData[i], data[i], $"Iteration {i}");
+            }
+            _databaseAdapter.AddData(SECOND_ADDITION);
+            data = _databaseAdapter.GetData();
+            for (int i = 0; i < data.Count; i++)
+            {
+                Debug.WriteLine(data[i]);
+                Assert.AreEqual(_possibleData[i], data[i], $"Iteration {i}");
+            }
         }
     }
 }
